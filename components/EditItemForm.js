a696bb3d0ccb754f515db5f179998c2f7f4384b6
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-04-24 21:04:05 -0700

    add handleEditItemFormSubmission function to EditItemForm.js

--- components/EditItemForm.js
+++ components/EditItemForm.js
@@ -4,3 +4,8 @@ import PropTypes from "prop-types";
 
 function EditItemForm(props){
   const { item } = props;
+
+  function handleEditItemFormSubmission(event) {
+    event.preventDefault();
+    props.onEditItem({names: event.target.names.value, origin: event.target.origin.value, description: event.target.description.value, id: item.id});
+  }
