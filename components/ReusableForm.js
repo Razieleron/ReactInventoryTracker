a064b4a1f9516923ee1aaa3189900309ca55de86
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-04-24 20:38:32 -0700

    WIP ReusableForm.js

---
+++ components/ReusableForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ReusableForm(props) {
+  return (
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+        <input
+          type='text'
+          name='names'
+          placeholder='Pair Names' />
+        <input
+          type='text'
+          name='origin'
+          placeholder='Origin of Blend' />
